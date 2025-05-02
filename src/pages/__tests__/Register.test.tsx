6794462c60bb6bc9a7e0c9ec391707ba190f7822
K0ord <kordianm03@gmail.com>
2025-05-01 20:53:51 -0400

    Fix Test cases

--- src/pages/__tests__/Register.test.tsx
+++ src/pages/__tests__/Register.test.tsx
@@ -1,13 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import Register from "../Register";
+import { ChakraProvider } from "@chakra-ui/react";
 import content from "../../locale/en";
+import { system } from "../../theme/theme";
+
+const config = {
+  ...system,
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
 
 describe("tests for the Register Page", () => {
   beforeAll(() => {
-    render(<Register />);
+    render(
+      <ChakraProvider value={config}>
+        <Register />
+      </ChakraProvider>,
+    );
   });
 
   it("should render the correct content", () => {
-    expect(screen.getByText(content.register.title)).toBeInTheDocument();
+    expect(screen.getByText(content.register.email)).toBeInTheDocument();
   });
 });
